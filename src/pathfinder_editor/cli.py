3256734896944fc2dba3364c2e3978848fadfe6b
adammartin <ebondark.od@gmail.com>
2018-10-04 15:57:16 -0400

    Playing around with extracting the files

--- src/pathfinder_editor/cli.py
+++ src/pathfinder_editor/cli.py
@@ -2,7 +2,7 @@ from pathfinder_editor_widget import PathfinderEditorWidget
 
 def pathfinder_editor():
     from pyforms import start_app
-    start_app(PathfinderEditorWidget)
+    start_app(PathfinderEditorWidget, geometry=(200, 200, 800, 200))
 
 
 if __name__ == '__main__':
--- src/pathfinder_editor/pathfinder_editor_widget.py
+++ src/pathfinder_editor/pathfinder_editor_widget.py
@@ -1,6 +1,8 @@
 from pyforms.basewidget import BaseWidget
 from pyforms.controls import ControlFile
 from pyforms.controls import ControlButton
+from pathlib import Path
+from zipfile import ZipFile
 
 class PathfinderEditorWidget(BaseWidget):
     def __init__(self, *args, **kwargs):
@@ -10,6 +12,16 @@ class PathfinderEditorWidget(BaseWidget):
         self._savebutton = ControlButton('Save')
 
         self._formset = [
-            ('_savefile', '_loadbutton'),
+            '_savefile',
+            '_loadbutton',
             '_savebutton',
         ]
+
+        self._loadbutton.changed_event = self.__load_save_file
+
+    def __load_save_file(self):
+        path = Path(self._savefile.value)
+        if path.is_file():
+            tempDir = Path('./tempdir')
+            with ZipFile(str(path.resolve()), 'r') as save_zip:
+                save_zip.extractall(path=str(tempDir.resolve()))
