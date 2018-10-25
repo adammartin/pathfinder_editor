e68becc9e3ec60494af757a0528cd160092c48aa
adammartin <ebondark.od@gmail.com>
2018-10-25 10:21:40 -0400

    Making the test about the main character

--- test/character/character_info_test.py
+++ test/character/character_info_test.py
@@ -11,29 +11,29 @@ MAIN_KEY = {
     'm_UniqueId': MAIN_CHAR_ID
 }
 
-
-def test_name():
-    party = pytest.helpers.party_base(MAIN_CHAR_ID, COMP_UNIT_ID, COMPANION_ID)
-    main_character = party['m_EntityData'][0]
-    character = CharacterInfo(party, MAIN_KEY)
-    assert character.name() == main_character['Descriptor']['CustomName']
-
-
-def test_alignment():
-    party = pytest.helpers.party_base(MAIN_CHAR_ID, COMP_UNIT_ID, COMPANION_ID)
-    character = CharacterInfo(party, MAIN_KEY)
-    assert character.alignment() == 'Neutral'
+def test_main_character():
+    def test_name():
+        party = pytest.helpers.party_base(MAIN_CHAR_ID, COMP_UNIT_ID, COMPANION_ID)
+        main_character = party['m_EntityData'][0]
+        character = CharacterInfo(party, MAIN_KEY)
+        assert character.name() == main_character['Descriptor']['CustomName']
 
 
-def test_update_alignment():
-    party = pytest.helpers.party_base(MAIN_CHAR_ID, COMP_UNIT_ID, COMPANION_ID)
-    character = CharacterInfo(party, MAIN_KEY)
-    main_character = party['m_EntityData'][0]
-    new_alignment = 'Neutral Good'
-    character.update_alignment(new_alignment)
-    alignment_data = main_character['Descriptor']['Alignment']
-    assert character.alignment() == new_alignment
-    assert alignment_data['Vector']['x'] == 0
-    assert alignment_data['Vector']['y'] == 1
-    assert alignment_data['m_History'][-1]['Position']['x'] == 0
-    assert alignment_data['m_History'][-1]['Position']['y'] == 1
+    def test_alignment():
+        party = pytest.helpers.party_base(MAIN_CHAR_ID, COMP_UNIT_ID, COMPANION_ID)
+        character = CharacterInfo(party, MAIN_KEY)
+        assert character.alignment() == 'Neutral'
+
+
+    def test_update_alignment():
+        party = pytest.helpers.party_base(MAIN_CHAR_ID, COMP_UNIT_ID, COMPANION_ID)
+        character = CharacterInfo(party, MAIN_KEY)
+        main_character = party['m_EntityData'][0]
+        new_alignment = 'Neutral Good'
+        character.update_alignment(new_alignment)
+        alignment_data = main_character['Descriptor']['Alignment']
+        assert character.alignment() == new_alignment
+        assert alignment_data['Vector']['x'] == 0
+        assert alignment_data['Vector']['y'] == 1
+        assert alignment_data['m_History'][-1]['Position']['x'] == 0
+        assert alignment_data['m_History'][-1]['Position']['y'] == 1
