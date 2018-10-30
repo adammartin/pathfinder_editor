48e7021f489c41348111b19470b255d9b6bc4058
adammartin <ebondark.od@gmail.com>
2018-10-30 09:41:33 -0400

    I can at this point get rid of the EntityInfo sub-classes and go more functional

--- src/editor/character/character_info.py
+++ src/editor/character/character_info.py
@@ -4,10 +4,15 @@ from editor.character.search_algorithms import id_matches, search_recursively
 
 class CharacterInfo(EntityInfo):
     def _find_character(self):
-        for entity in self._party_data['m_EntityData']:
-            if _has_unique_id(entity, self._key['m_UniqueId']):
-                return _search_for_player(entity)
-        return None
+        return _search_for_main_character(self._party_data,
+                                          self._key)
+
+
+def _search_for_main_character(party, key):
+    for entity in party['m_EntityData']:
+        if _has_unique_id(entity, key['m_UniqueId']):
+            return _search_for_player(entity)
+    return None
 
 
 def _has_unique_id(entity, unique_id):
