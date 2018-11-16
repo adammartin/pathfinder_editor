3aec7f84edf49f09f6141019db9be6647e034cc1
adammartin <ebondark.od@gmail.com>
2018-11-16 09:09:37 -0500

    Adding a modification to build scrip that will build both version same time

--- bin/build_editor.sh
+++ bin/build_editor.sh
@@ -9,3 +9,12 @@ set -e
 
 with_venv
 pyinstaller -w -F --clean "$PROJECT_DIR/src/$PYTHON_PACKAGE_NAME/pathfinder_editor.py"
+
+rm -rf "$PROJECT_DIR/pathfinder_editor_win"
+mkdir "$PROJECT_DIR/pathfinder_editor_win"
+cp -R "$PROJECT_DIR/winpython" "$PROJECT_DIR/pathfinder_editor_win/winpython"
+cp -R "$PROJECT_DIR/src/editor" "$PROJECT_DIR/pathfinder_editor_win/editor"
+cp -R "$PROJECT_DIR/src/pathfinder_launcher.bat" "$PROJECT_DIR/pathfinder_editor_win/pathfinder_launcher.bat"
+cp "$PROJECT_DIR/pathfinder_editor_win/editor/pathfinder_editor.py" "$PROJECT_DIR/pathfinder_editor_win/pathfinder_editor.py"
+cd "$PROJECT_DIR/pathfinder_editor_win"
+zip -r "$PROJECT_DIR/dist/pathfinder_editor_win.zip" "."
