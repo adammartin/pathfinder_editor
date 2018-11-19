8a35b8f6124cb713b3aac9dceb629f5f3718a194
adammartin <ebondark.od@gmail.com>
2018-11-19 16:20:41 -0500

    Adding option menu

--- src/editor/widgets/fields/field.py
+++ src/editor/widgets/fields/field.py
@@ -4,12 +4,28 @@ from editor.widgets.fields import label
 
 
 class GridField():
-    def __init__(self, panel, a_row, a_column, label_text):
+    def __init__(self, panel, a_row, a_column, label_text, event_function):
         actual_col = a_column*2
         self._label = label.GridLabel(panel, a_row, actual_col, label_text)
         self._field = tkinter.StringVar()
+        self._field.trace('w', event_function)
         self._entry = tkinter.Entry(panel, textvariable=self._field)
         self._entry.grid(row=a_row, column=actual_col+1, sticky=W)
+
+    def update(self, function, locked):
+        if self._field.get() and not locked:
+            function(self._field.get())
+
+
+class GridOptionMenu():
+    def __init__(self, panel, a_row, a_column, label_text, choices, event_function):
+        # pylint: disable=too-many-arguments
+        actual_col = a_column*2
+        self._label = label.GridLabel(panel, a_row, actual_col, label_text)
+        self._field = tkinter.StringVar()
+        self._field.trace('w', event_function)
+        self._option_menu = tkinter.OptionMenu(panel, self._field, *choices)
+        self._option_menu.grid(row=a_row, column=actual_col+1, sticky=W)
 
     def update(self, function, locked):
         if self._field.get() and not locked:
--- test/widgets/fields/grid_field_test.py
+++ test/widgets/fields/grid_field_test.py
@@ -4,39 +4,25 @@ from tkinter import W
 from unittest import mock
 from unittest.mock import patch
 from editor.widgets.fields import label
-from editor.widgets.fields.field import GridField
+from editor.widgets.fields.field import GridField, GridOptionMenu
 
 
 
 ROW = mock.MagicMock()
 COLUMN = mock.MagicMock()
+EVENT_FUNCTION = mock.MagicMock()
+CHOICES = ['foo', 'bar']
 LABEL_TEXT = "SOME LABEL_TEXT"
 PANEL = mock.MagicMock()
 LOCKED = True
 UNLOCKED = False
 
 
-
-#    def _add_field(self, a_row, a_col, label_text, function):
-#        col = a_col*2
-#        self._add_label(a_row, col, label_text)
-#        variable = StringVar()
-#        variable.trace('w', function)
-#        entry = Entry(self._panel, textvariable=variable)
-#        entry.grid(row=a_row, column=col+1, sticky=W)
-#        return variable
-
-#    def _update(self, field, function):
-#        if field.get() and not self._dirty_lock:
-#            function(field.get())
-
-
 @patch('tkinter.Entry')
 @patch('tkinter.StringVar')
 @patch('editor.widgets.fields.label.GridLabel')
 def test_field_properly_composed(mock_label, mock_var, mock_entry):
-    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT)
-    mock_var.assert_called()
+    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT, EVENT_FUNCTION)
     var_instance = mock_var.return_value
     mock_label.assert_called_with(PANEL, ROW, COLUMN*2, LABEL_TEXT)
     mock_entry.assert_called_with(PANEL, textvariable=var_instance)
@@ -46,8 +32,17 @@ def test_field_properly_composed(mock_label, mock_var, mock_entry):
 @patch('tkinter.StringVar')
 @patch('editor.widgets.fields.label.GridLabel')
 def test_field_positioned_on_grid(mock_label, mock_var, mock_entry):
-    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT)
+    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT, EVENT_FUNCTION)
     mock_entry.return_value.grid.assert_called_with(row=ROW, column=(COLUMN*2)+2, sticky=W)
+
+
+@patch('tkinter.Entry')
+@patch('tkinter.StringVar')
+@patch('editor.widgets.fields.label.GridLabel')
+def test_field_will_properly_be_updated_by_entry(mock_label, mock_var, mock_entry):
+    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT, EVENT_FUNCTION)
+    var_instance = mock_var.return_value
+    var_instance.trace.assert_called_with('w', EVENT_FUNCTION)
 
 
 @patch('tkinter.Entry')
@@ -55,7 +50,7 @@ def test_field_positioned_on_grid(mock_label, mock_var, mock_entry):
 @patch('editor.widgets.fields.label.GridLabel')
 def test_field_updates_function(mock_label, mock_var, mock_entry):
     function = mock.MagicMock()
-    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT)
+    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT, EVENT_FUNCTION)
     expected_value = '5'
     mock_var.return_value.get.return_value = expected_value
     field.update(function, UNLOCKED)
@@ -67,7 +62,7 @@ def test_field_updates_function(mock_label, mock_var, mock_entry):
 @patch('editor.widgets.fields.label.GridLabel')
 def test_field_updates_function_if_not_None(mock_label, mock_var, mock_entry):
     function = mock.MagicMock()
-    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT)
+    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT, EVENT_FUNCTION)
     expected_value = None
     mock_var.return_value.get.return_value = expected_value
     field.update(function, UNLOCKED)
@@ -79,7 +74,70 @@ def test_field_updates_function_if_not_None(mock_label, mock_var, mock_entry):
 @patch('editor.widgets.fields.label.GridLabel')
 def test_field_updates_function_if_not_None(mock_label, mock_var, mock_entry):
     function = mock.MagicMock()
-    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT)
+    field = GridField(PANEL, ROW, COLUMN, LABEL_TEXT, EVENT_FUNCTION)
+    expected_value = '5'
+    mock_var.return_value.get.return_value = expected_value
+    field.update(function, LOCKED)
+    function.assert_not_called()
+
+
+@patch('tkinter.OptionMenu')
+@patch('tkinter.StringVar')
+@patch('editor.widgets.fields.label.GridLabel')
+def test_option_properly_composed(mock_label, mock_var, mock_option):
+    field = GridOptionMenu(PANEL, ROW, COLUMN, LABEL_TEXT, CHOICES, EVENT_FUNCTION)
+    var_instance = mock_var.return_value
+    mock_label.assert_called_with(PANEL, ROW, COLUMN*2, LABEL_TEXT)
+    mock_option.assert_called_with(PANEL, var_instance, *CHOICES)
+
+
+@patch('tkinter.OptionMenu')
+@patch('tkinter.StringVar')
+@patch('editor.widgets.fields.label.GridLabel')
+def test_option_positioned_on_grid(mock_label, mock_var, mock_option):
+    field = GridOptionMenu(PANEL, ROW, COLUMN, LABEL_TEXT, CHOICES, EVENT_FUNCTION)
+    mock_option.return_value.grid.assert_called_with(row=ROW, column=(COLUMN*2)+2, sticky=W)
+
+
+@patch('tkinter.OptionMenu')
+@patch('tkinter.StringVar')
+@patch('editor.widgets.fields.label.GridLabel')
+def test_field_will_properly_be_updated_by_entry(mock_label, mock_var, mock_option):
+    field = GridOptionMenu(PANEL, ROW, COLUMN, LABEL_TEXT, CHOICES, EVENT_FUNCTION)
+    var_instance = mock_var.return_value
+    var_instance.trace.assert_called_with('w', EVENT_FUNCTION)
+
+
+@patch('tkinter.OptionMenu')
+@patch('tkinter.StringVar')
+@patch('editor.widgets.fields.label.GridLabel')
+def test_field_updates_function(mock_label, mock_var, mock_option):
+    function = mock.MagicMock()
+    field = GridOptionMenu(PANEL, ROW, COLUMN, LABEL_TEXT, CHOICES, EVENT_FUNCTION)
+    expected_value = '5'
+    mock_var.return_value.get.return_value = expected_value
+    field.update(function, UNLOCKED)
+    function.assert_called_with(expected_value)
+
+
+@patch('tkinter.OptionMenu')
+@patch('tkinter.StringVar')
+@patch('editor.widgets.fields.label.GridLabel')
+def test_field_updates_function_if_not_None(mock_label, mock_var, mock_option):
+    function = mock.MagicMock()
+    field = GridOptionMenu(PANEL, ROW, COLUMN, LABEL_TEXT, CHOICES, EVENT_FUNCTION)
+    expected_value = None
+    mock_var.return_value.get.return_value = expected_value
+    field.update(function, UNLOCKED)
+    function.assert_not_called()
+
+
+@patch('tkinter.OptionMenu')
+@patch('tkinter.StringVar')
+@patch('editor.widgets.fields.label.GridLabel')
+def test_field_updates_function_if_not_None(mock_label, mock_var, mock_option):
+    function = mock.MagicMock()
+    field = GridOptionMenu(PANEL, ROW, COLUMN, LABEL_TEXT, CHOICES, EVENT_FUNCTION)
     expected_value = '5'
     mock_var.return_value.get.return_value = expected_value
     field.update(function, LOCKED)
